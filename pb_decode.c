eab4151a99f0c2fc2f41c51356d04898255ccbfe
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2013-01-16 16:32:48 +0200

    Oops, typoed #ifdef; again fixing previous commit.

--- pb_decode.c
+++ pb_decode.c
@@ -226,7 +226,7 @@ void pb_close_string_substream(pb_istream_t *stream, pb_istream_t *substream)
 {
     stream->state = substream->state;
 
-#ifdef PB_NO_ERRMSG
+#ifndef PB_NO_ERRMSG
     stream->errmsg = substream->errmsg;
 #endif
 }
