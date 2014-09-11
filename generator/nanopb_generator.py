62bbe46a4555df35a7a07f347b42237bdf7608b5
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2014-09-11 18:58:16 +0300

    Publishing nanopb-0.1.9.1

--- generator/nanopb_generator.py
+++ generator/nanopb_generator.py
@@ -1,5 +1,5 @@
 '''Generate header file for nanopb from a ProtoBuf FileDescriptorSet.'''
-nanopb_version = "nanopb-0.1.9.1-dev"
+nanopb_version = "nanopb-0.1.9.1"
 
 try:
     import google.protobuf.descriptor_pb2 as descriptor
