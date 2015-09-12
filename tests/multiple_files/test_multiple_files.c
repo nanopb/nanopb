936cfdc675c2dc3580c2459e8b1773a1d0bf9a8b
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2015-09-12 13:27:56 +0300

    Expand the multiple_files test case to include oneofs and enums

--- tests/multiple_files/test_multiple_files.c
+++ tests/multiple_files/test_multiple_files.c
@@ -4,9 +4,19 @@
 
 #include <stdio.h>
 #include <pb_encode.h>
-#include "callbacks2.pb.h"
+#include "unittests.h"
+#include "multifile2.pb.h"
+
+/* Check that the size definition is obtained properly */
+static const int foo = OneofMessage_size;
 
 int main()
 {
-	return 0;
+    int status = 0;
+    
+    /* Check that enum signedness is detected properly */
+    TEST(PB_LTYPE(Enums_fields[0].type) == PB_LTYPE_VARINT);
+    TEST(PB_LTYPE(Enums_fields[0].type) == PB_LTYPE_UVARINT);
+    
+    return status;
 }
