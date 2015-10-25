6c136580a091a30e94aaebd66dd978117d999ad0
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2015-10-25 18:07:23 +0200

    Ignore null pointers in pb_release() (issue #183).

--- pb_decode.c
+++ pb_decode.c
@@ -1067,6 +1067,9 @@ void pb_release(const pb_field_t fields[], void *dest_struct)
 {
     pb_field_iter_t iter;
     
+    if (!dest_struct)
+        return; /* Ignore NULL pointers, similar to free() */
+
     if (!pb_field_iter_begin(&iter, fields, dest_struct))
         return; /* Empty message type */
     
