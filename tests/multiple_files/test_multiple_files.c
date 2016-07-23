298d00e8d73c23b8b2537489aee4319a43968e9e
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2016-07-23 20:24:54 +0300

    Include package name in include guard (issue #207).
    Fix suggested by Ulenspiegel.
    
    Also added testcase for the same.

--- tests/multiple_files/test_multiple_files.c
+++ tests/multiple_files/test_multiple_files.c
@@ -6,6 +6,7 @@
 #include <pb_encode.h>
 #include "unittests.h"
 #include "multifile2.pb.h"
+#include "subdir/multifile2.pb.h"
 
 int main()
 {
@@ -17,6 +18,13 @@ int main()
     /* Check that enum signedness is detected properly */
     TEST(PB_LTYPE(Enums_fields[0].type) == PB_LTYPE_VARINT);
     TEST(PB_LTYPE(Enums_fields[1].type) == PB_LTYPE_UVARINT);
+    
+    /* Test that subdir file is correctly included */
+    {
+        subdir_SubdirMessage foo = subdir_SubdirMessage_init_default;
+        TEST(foo.foo == 15);
+        /* TEST(subdir_OneofMessage_size == 27); */ /* TODO: Issue #172 */
+    }
     
     return status;
 }
