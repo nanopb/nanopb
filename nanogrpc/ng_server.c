145d7266be09b28d8623227d4b3562ab199dc47f
Zdzisław Krajewski <zdzichucb@gmail.com>
2017-04-24 22:12:50 +0200

    Added encoding error messages, some checkings in functions, changed context managing

--- nanogrpc/ng_server.c
+++ nanogrpc/ng_server.c
@@ -9,6 +9,40 @@
 static DEFINE_FILL_WITH_ZEROS_FUNCTION(GrpcRequest)
 static DEFINE_FILL_WITH_ZEROS_FUNCTION(GrpcResponse)
 
+
+#include <string.h>
+
+const char* errorMessages[] = {
+  "Callback failed",
+  "Unable to decode message from request holder",
+  "Unable to register call",
+  "callback not found",
+  "No method found"
+};
+
+
+
+enum grpcError  {
+  GrpcErrorMsg_callback_failed = 0,
+  GrpcErrorMsg_unable_to_decode_message,
+  GrpcErrorMsg_unable_to_register_call,
+  GrpcErrorMsg_callback_not_found,
+  GrpcErrorMsg_no_method_found
+};
+
+
+static bool encodeErrorMessageCallback(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
+{
+  uint32_t index = *(uint32_t*)arg;
+  /* char *str = get_string_from_somewhere(); */
+  if (!pb_encode_tag_for_field(stream, field))
+      return false;
+  /* we are encoding tag for bytes, but writeing submessage, */
+  /* as long it is prepended with size same as bytes */
+  /*return pb_encode_submessage(stream, method->response_fields, method->response_holder);*/
+  return  pb_encode_string(stream, (pb_byte_t*) errorMessages[index], strlen(errorMessages[index]));
+}
+
 /*!
  * @brief Returns method with given hash.
  *
@@ -77,7 +111,7 @@ static ng_method_t * getMethodByPath(ng_grpc_handle_t *handle, char * path){
   }
 
   /* in order to compare names we could use strcmp, but it requires
-   * zero terminated string, but if we dpn't want to insert zeros into
+   * zero terminated string, but if we don't want to insert zeros into
    * given string we have to use strncmp and compare names lengths. */
   while (service != NULL){ /* Iterate over services */
     if (strncmp(service->name, serviceName, serviceNameLenth) == 0 &&
@@ -148,8 +182,10 @@ bool ng_GrpcParseBlocking(ng_grpc_handle_t *handle){
   GrpcResponse_fillWithZeros(&handle->response);
 
   if (pb_decode(handle->input, GrpcRequest_fields, &handle->request)){
+    pb_istream_t input;
+    input = pb_istream_from_buffer(handle->request.data->bytes, handle->request.data->size);
+
     handle->response.call_id = handle->request.call_id;
-    pb_istream_t input = pb_istream_from_buffer(handle->request.data->bytes, handle->request.data->size);
 
     /* look for method by hash only if it has been provided */
     if (handle->request.path_hash != 0){
@@ -244,6 +280,15 @@ bool ng_GrpcParseBlocking(ng_grpc_handle_t *handle){
  * @return         true if successfully added method, fasle if not
  */
 bool ng_addMethodToService(ng_service_t * service, ng_method_t * method){
+  /* Check if method isn't already registered */
+  ng_method_t* temp = service->method;
+  while (temp != NULL){
+    if (temp == method){
+      return false;
+    }
+    temp = temp->next;
+  }
+
   if (method != NULL && service != NULL){
     method->next = service->method;
     service->method = method;
@@ -256,8 +301,16 @@ bool ng_addMethodToService(ng_service_t * service, ng_method_t * method){
 
 
 bool ng_setMethodContext(ng_method_t* method, ng_methodContext_t* ctx){
+  ng_methodContext_t* temp = method->context;
+  while (temp != NULL){
+    if (temp == ctx){
+      return false;
+    }
+    temp = temp->next;
+  }
   if (ctx != NULL){
     method->context = ctx;
+    ctx->method = method;
     return true;
   } else {
     return false;
@@ -287,6 +340,15 @@ bool ng_setMethodCallback(ng_method_t *method,
  * @return         true if success, false if not
  */
 bool ng_GrpcRegisterService(ng_grpc_handle_t *handle, ng_service_t * service){
+  /* Check if service isn't already registered */
+  ng_service_t* temp = handle->serviceHolder;
+  while (temp != NULL){
+    if (temp == service){
+      return false;
+    }
+    temp = temp->next;
+  }
+
   if (service != NULL && handle != NULL){
     service->next = handle->serviceHolder;
     handle->serviceHolder = service;
@@ -327,6 +389,10 @@ static bool ng_registerCall(ng_grpc_handle_t *handle, ng_methodContext_t *ctx, n
 
 static bool ng_removeCall(ng_grpc_handle_t *handle, ng_callId_t id){
   uint32_t i;
+  if (id == 0){
+    return false;
+  }
+
   for (i=0; i< handle->callsHolderSize; i++){
     if (handle->callsHolder[i].call_id == id){
       handle->callsHolder[i].call_id = 0;
@@ -357,24 +423,31 @@ bool ng_isContextUsedByOngoingCall(ng_grpc_handle_t* handle, ng_methodContext_t*
  * @param  method pointer to method
  * @return        pointer to valid context, NULL if no such found
  */
-ng_methodContext_t* ng_getValidContextFromMethod(ng_grpc_handle_t* handle, ng_method_t* method){
+static ng_methodContext_t* ng_getValidContext(ng_grpc_handle_t* handle, ng_method_t* method){
   /* For now we are returning only first context, but just in case we are
     checking if it is not being used by some ongoing call. In future in case
     of having more contexts per method this function will handle providing
     correct context (or something like this :) ) */
-    if (method->context != NULL){
-      if (method->context->request != NULL &&
-          method->context->response != NULL){
-        /*if (ng_isContextUsedByOngoingCall(handle, method->context)){
-          return NULL;
-        } else {
-          return method->context;
-        } */
-    	return method->context;
-      }
-    }
+  if (method->context != NULL){
+    if (method->context->request != NULL &&
+        method->context->response != NULL){
+      /*if (ng_isContextUsedByOngoingCall(handle, method->context)){
+        ng_removeCall();
+        return NULL;
+      } else {
+        return method->context;
+      }*/
 
-    return NULL;
+      /* We could chekc if context is not being used by some call,
+         but let's remove it. Removeing call with id = 0 won't do anyhing.
+         just return false, but that's fine. */
+      ng_removeCall(handle, method->context->call_id);
+      /*method->context->method = method; */ /* moved to ng_setMethodContext */
+    	return method->context;
+    }
+  }
+
+  return NULL;
 }
 
 /*!
@@ -386,6 +459,7 @@ bool ng_GrpcParseNonBlocking(ng_grpc_handle_t* handle){
   ng_CallbackStatus_t status;
   ng_method_t *method = NULL;
   ng_methodContext_t* ctx = NULL;
+  uint32_t error = 0;
   bool sendNow = true;
   if (handle->input == NULL || handle->output == NULL ||
       handle->canIWriteToOutput == NULL || handle->outputReady == NULL){
@@ -409,52 +483,72 @@ bool ng_GrpcParseNonBlocking(ng_grpc_handle_t* handle){
     }
 
     if (method != NULL) {
-      if (method->callback != NULL &&
-        ng_getValidContextFromMethod(handle, method) != NULL){ /* callback and context found */
-        ctx = method->context;
-        ctx->method = method;
-        if (ng_registerCall(handle, ctx, call_id)){
-          method->request_fillWithZeros(ctx->request);
-          if (pb_decode(&input, method->request_fields, ctx->request)){
-            ctx->call_id = call_id;
-            method->response_fillWithZeros(ctx->response);
-            status = method->callback(ctx);
+      if (method->callback != NULL){
+        ctx = ng_getValidContext(handle, method);
+        if (ctx != NULL){ /* context found */
+          /* ctx = method->context;
+          ctx->method = method; */ /* TODO should that be assigned here? theoretically
+                                  context should all the time belong to method, but
+                                  in case of creating contexts dynamically we should
+                                  not bother here about setting it. Lets just obtain
+                                  valid context for method? */
+          if (ng_registerCall(handle, ctx, call_id)){
+            method->request_fillWithZeros(ctx->request);
+            if (pb_decode(&input, method->request_fields, ctx->request)){
+              ctx->call_id = call_id;
+              method->response_fillWithZeros(ctx->response);
+              status = method->callback(ctx);
 
-            if (status == CallbackStatus_Ok ||
-                status == CallbackStatus_WillContinueLater){
-              bool validResponse = false;
-              size_t responseSize;
-              /* try to encode method response to make sure, that it will be
-              * possible to encode it callback.  */
-              validResponse = pb_get_encoded_size(&responseSize,
-                                                  method->response_fields,
-                                                  ctx->response);
-              if (validResponse){
-                handle->response.grpc_status = GrpcStatus_OK;
-                handle->response.data.funcs.encode = &encodeResponseCallback;
-                handle->response.data.arg = ctx;
-              } else {
+              if (status == CallbackStatus_Ok ||
+                  status == CallbackStatus_WillContinueLater){
+                bool validResponse = false;
+                size_t responseSize;
+                /* try to encode method response to make sure, that it will be
+                * possible to encode it callback.  */
+                validResponse = pb_get_encoded_size(&responseSize,
+                                                    method->response_fields,
+                                                    ctx->response);
+                if (validResponse){
+                  handle->response.grpc_status = GrpcStatus_OK;
+                  handle->response.data.funcs.encode = &encodeResponseCallback;
+                  handle->response.data.arg = ctx;
+                } else {
+                  handle->response.grpc_status = GrpcStatus_INTERNAL;
+                  /* TODO insert here message about not being able to
+                  * encode method request? */
+                }
+                if (status == CallbackStatus_Ok || method->server_streaming == false){
+                  handle->response.has_response_type = true;
+                  handle->response.response_type = GrpcResponseType_END_OF_CALL;
+                  ng_removeCall(handle, call_id);
+                }
+              } else if (status == CallbackStatus_WillRespondLater){
+                sendNow = false;
+              } else { /* callback failed. */
                 handle->response.grpc_status = GrpcStatus_INTERNAL;
-                /* TODO insert here message about not being able to
-                * encode method request? */
+                handle->response.grpc_mesage.funcs.encode = &encodeErrorMessageCallback;
+                error = GrpcErrorMsg_callback_failed;
+                handle->response.grpc_mesage.arg = &error;
               }
-              if (status == CallbackStatus_Ok || method->server_streaming == false){
-                handle->response.has_response_type = true;
-                handle->response.response_type = GrpcResponseType_END_OF_CALL;
-                ng_removeCall(handle, call_id);
-              }
-            } else if (status == CallbackStatus_WillRespondLater){
-              sendNow = false;
-            } else { /* callback failed. */
-              handle->response.grpc_status = GrpcStatus_INTERNAL;
+           } else { /* unable to decode message from request holder */
+              /* We registered it previously but since decoding failed
+                 we have to remove it since we won't be able to respond */
+              ng_removeCall(handle, call_id);
+              handle->response.grpc_status = GrpcStatus_INVALID_ARGUMENT;
+              handle->response.grpc_mesage.funcs.encode = &encodeErrorMessageCallback;
+              error = GrpcErrorMsg_unable_to_decode_message;
+      		    handle->response.grpc_mesage.arg = &error;
             }
-         } else { /* unable to decode message from request holder */
-            handle->response.grpc_status = GrpcStatus_INVALID_ARGUMENT;
+          } else { /* Unable to register call */
+            handle->response.grpc_status = GrpcStatus_INTERNAL;
+            handle->response.grpc_mesage.funcs.encode = &encodeErrorMessageCallback;
+            error = GrpcErrorMsg_unable_to_register_call;
+      		  handle->response.grpc_mesage.arg = &error;
           }
-        } else { /* Unable to register call */
+        } else { /* no available context */
           handle->response.grpc_status = GrpcStatus_INTERNAL;
         }
-      } else { /* handler or callback not found */
+      } else { /* callback not found */
         handle->response.grpc_status = GrpcStatus_UNIMPLEMENTED;
       }
     } else { /* No method found*/
