29440cd9b9000a38ad6f6d91d1b9c7be1f7cd50c
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2018-11-10 14:45:20 +0200

    Automatically rebuild nanopb_pb2.py

---
+++ generator/proto/__init__.py
@@ -0,0 +1,19 @@
+'''This file automatically rebuilds the proto definitions for Python.'''
+
+import os
+import os.path
+import sys
+import subprocess
+
+dirname = os.path.dirname(__file__)
+protosrc = os.path.join(dirname, "nanopb.proto")
+protodst = os.path.join(dirname, "nanopb_pb2.py")
+
+if os.path.isfile(protosrc):
+    src_date = os.path.getmtime(protosrc)
+    if not os.path.isfile(protodst) or os.path.getmtime(protodst) < src_date:
+        cmd = ["protoc", "--python_out=.", "nanopb.proto", "plugin.proto"]
+        status = subprocess.call(cmd, cwd = dirname)
+        if status != 0:
+            sys.stderr.write("Failed to build nanopb_pb2.py: " + ' '.join(cmd) + "\n")
+
