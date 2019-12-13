93c58e344fd5b1b4c86524a1c731389dacb51954
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2019-12-13 21:56:56 +0200

    Fix pip package

--- extra/poetry/poetry_build.sh
+++ extra/poetry/poetry_build.sh
@@ -2,8 +2,8 @@
 
 rm -rf nanopb
 mkdir nanopb
-touch nanopb/__init__.py
 cp -pr ../../generator nanopb/
+touch nanopb/__init__.py nanopb/generator/__init__.py
 cp -pr ../../README.md .
 sed -i -e 's/\(version =.*\)-dev.*/\1-dev'$(git rev-list HEAD --count)'"/' pyproject.toml
 poetry build
