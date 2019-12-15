61387d4a324fd9ceaad42726ec9011a430295c01
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2019-12-15 18:21:19 +0200

    STM32: connect under reset

--- tests/site_scons/platforms/stm32/run_test.sh
+++ tests/site_scons/platforms/stm32/run_test.sh
@@ -8,8 +8,10 @@ ARGS=$*
 test X$OPENOCD_BOARD == X && export OPENOCD_BOARD=board/stm32f7discovery.cfg
 
 openocd -f $OPENOCD_BOARD \
+    -c "reset_config srst_only srst_nogate connect_assert_srst" \
     -c "init" -c "arm semihosting enable" \
     -c "arm semihosting_cmdline $BASENAME $ARGS" \
+    -c "reset halt" \
     -c "load_image $BINARY 0" \
     -c "reset halt" -c "resume 0x20000000" 2>openocd.log
 
