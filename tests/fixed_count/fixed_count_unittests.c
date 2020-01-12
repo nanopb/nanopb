37aa79a2f661c036129cfe559f26265763f07418
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2020-01-12 13:31:21 +0200

    Add tests for error detection in fixed size arrays.

--- tests/fixed_count/fixed_count_unittests.c
+++ tests/fixed_count/fixed_count_unittests.c
@@ -8,7 +8,6 @@
 int main()
 {
     int status = 0;
-    COMMENT("Test encoding and decoding of repeated fixed count fields");
 
     {
       pb_byte_t buffer[Message1_size];
@@ -23,6 +22,8 @@ int main()
       msg_a.data[0] = 2;
       msg_a.data[0] = 3;
 
+      COMMENT("Test encode and decode with three entries");
+
       ostream = pb_ostream_from_buffer(buffer, Message1_size);
       TEST(pb_encode(&ostream, Message1_fields, &msg_a));
       message_length = ostream.bytes_written;
@@ -35,6 +36,26 @@ int main()
     }
 
     {
+      pb_byte_t input[] = {0x08, 0x00, 0x08, 0x01, 0x08, 0x02, 0x08, 0x03};
+      pb_istream_t stream;
+      Message1 msg = Message1_init_zero;
+
+      COMMENT("Test wrong number of entries");
+
+      stream = pb_istream_from_buffer(input, 6);
+      TEST(pb_decode(&stream, Message1_fields, &msg));
+      TEST(msg.data[0] == 0 && msg.data[1] == 1 && msg.data[2] == 2);
+
+      stream = pb_istream_from_buffer(input, 8);
+      TEST(!pb_decode(&stream, Message1_fields, &msg));
+      TEST(strcmp(stream.errmsg, "array overflow") == 0);
+
+      stream = pb_istream_from_buffer(input, 4);
+      TEST(!pb_decode(&stream, Message1_fields, &msg));
+      TEST(strcmp(stream.errmsg, "wrong size for fixed count field") == 0);
+    }
+
+    {
       pb_byte_t buffer[Message2_size];
       Message2 msg_a = Message2_init_zero;
       Message2 msg_b = Message2_init_zero;
@@ -42,6 +63,8 @@ int main()
       pb_ostream_t ostream;
       pb_istream_t istream;
       size_t message_length;
+
+      COMMENT("Test encode and decode with nested messages");
 
       msg_a.data[0].data[0] = 1;
       msg_a.data[0].data[1] = 2;
@@ -69,6 +92,8 @@ int main()
       pb_ostream_t ostream;
       pb_istream_t istream;
       size_t message_length;
+
+      COMMENT("Test encode and decode with two nested messages");
 
       msg_a.data1[0].data[0].data[0] = 1;
       msg_a.data1[0].data[0].data[1] = 2;
