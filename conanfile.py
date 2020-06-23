e6824871fb3efca722982210cb48e1c0384cb518
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2020-06-23 15:20:15 +0300

    Setting version to 0.4.3-dev

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from os import path
 
 class NanoPbConan(ConanFile):
     name = "nanopb"
-    version = "0.4.2"
+    version = "0.4.3-dev"
     license = "zlib"
     url = "https://jpa.kapsi.fi/nanopb/"
     description = "Protocol Buffers with small code size"
