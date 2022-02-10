40c72efff2130e8301f0c43357f3ba0dafea7420
Gil Shapira <gil.shapira@intusurg.com>
2022-02-10 11:26:57 -0800

    Use memcpy instead of iterating on buf_read/write

--- pb_decode.c
+++ pb_decode.c
@@ -67,14 +67,12 @@ typedef struct {
 
 static bool checkreturn buf_read(pb_istream_t *stream, pb_byte_t *buf, size_t count)
 {
-    size_t i;
     const pb_byte_t *source = (const pb_byte_t*)stream->state;
     stream->state = (pb_byte_t*)stream->state + count;
     
     if (buf != NULL)
     {
-        for (i = 0; i < count; i++)
-            buf[i] = source[i];
+        memcpy(buf, source, count * sizeof(pb_byte_t));
     }
     
     return true;
