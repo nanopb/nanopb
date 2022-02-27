391ba515900cc04a97a7f17ef11178b5920f47bc
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2022-02-27 13:38:26 +0200

    Rebuild nanopb_pb2.py and print version numbers on import failure (#733, #742)
    When python-protobuf and protoc versions do not match, the most common
    error message is "AttributeError: module 'google.protobuf.descriptor' has no attribute '_internal_create_key'".
    Attempt to provide more helpful info in this situation.

--- generator/proto/__init__.py
+++ generator/proto/__init__.py
@@ -6,31 +6,45 @@ import sys
 
 import pkg_resources
 
-from ._utils import has_grpcio_protoc, invoke_protoc
+from ._utils import has_grpcio_protoc, invoke_protoc, print_versions
 
 dirname = os.path.dirname(__file__)
 protosrc = os.path.join(dirname, "nanopb.proto")
 protodst = os.path.join(dirname, "nanopb_pb2.py")
+rebuild = False
 
 if os.path.isfile(protosrc):
     src_date = os.path.getmtime(protosrc)
     if not os.path.isfile(protodst) or os.path.getmtime(protodst) < src_date:
+        rebuild = True
 
-        cmd = [
-            "protoc",
-            "--python_out={}".format(dirname),
-            protosrc,
-            "-I={}".format(dirname),
-        ]
+if not rebuild:
+    try:
+        from . import nanopb_pb2
+    except AttributeError as e:
+        rebuild = True
+        sys.stderr.write("Failed to import nanopb_pb2.py: " + str(e) + "\n"
+                         "Will automatically attempt to rebuild this.\n"
+                         "Verify that python-protobuf and protoc versions match.\n")
+        print_versions()
 
-        if has_grpcio_protoc():
-            # grpcio-tools has an extra CLI argument
-            # from grpc.tools.protoc __main__ invocation.
-            _builtin_proto_include = pkg_resources.resource_filename('grpc_tools', '_proto')
+if rebuild:
+    cmd = [
+        "protoc",
+        "--python_out={}".format(dirname),
+        protosrc,
+        "-I={}".format(dirname),
+    ]
 
-            cmd.append("-I={}".format(_builtin_proto_include))
-        try:
-            invoke_protoc(argv=cmd)
-        except:
-            sys.stderr.write("Failed to build nanopb_pb2.py: " + ' '.join(cmd) + "\n")
-            raise
+    if has_grpcio_protoc():
+        # grpcio-tools has an extra CLI argument
+        # from grpc.tools.protoc __main__ invocation.
+        _builtin_proto_include = pkg_resources.resource_filename('grpc_tools', '_proto')
+
+        cmd.append("-I={}".format(_builtin_proto_include))
+    try:
+        invoke_protoc(argv=cmd)
+    except:
+        sys.stderr.write("Failed to build nanopb_pb2.py: " + ' '.join(cmd) + "\n")
+        raise
+
