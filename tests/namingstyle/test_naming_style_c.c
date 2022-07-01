5bdde20dae687a3b8d67ff0373fb35a4b05ae0ad
Krzysztof Rosinski <krzysiek@jrdltd.co.uk>
2022-07-01 20:10:30 +0200

    add namingstyle test placeholder

---
+++ tests/namingstyle/test_naming_style_c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "unittests.h"
+#include "naming_style.pb.h"
+
+int main()
+{
+    simple_message_t message = SIMPLE_MESSAGE_INIT_ZERO;
+    message.lucky_number = 13;
+    return 0;
+}
