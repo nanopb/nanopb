da6f1c607654d34dfba9a7e78a252ff717e35c89
Krzysztof Rosinski <krzysiek@jrdltd.co.uk>
2022-07-02 09:56:40 +0200

    update C style example

--- examples/c_style/c_style.c
+++ examples/c_style/c_style.c
@@ -9,7 +9,7 @@ int main()
     uint8_t buffer[128];
     size_t message_length;
     bool status;
-    
+
     /* Encode our message */
     {
         /* Allocate space on the stack to store the message data.
@@ -20,17 +20,17 @@ int main()
          * so that you do not have garbage data from RAM in there.
          */
         simple_message_t message = SIMPLE_MESSAGE_INIT_ZERO;
-        
+
         /* Create a stream that will write to our buffer. */
         pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-        
+
         /* Fill in the lucky number */
         message.lucky_number = 13;
-        
+
         /* Now we are ready to encode the message! */
         status = pb_encode(&stream, SIMPLE_MESSAGE_FIELDS, &message);
         message_length = stream.bytes_written;
-        
+
         /* Then just check for any errors.. */
         if (!status)
         {
@@ -38,34 +38,33 @@ int main()
             return 1;
         }
     }
-    
+
     /* Now we could transmit the message over network, store it in a file or
      * wrap it to a pigeon's leg.
      */
 
     /* But because we are lazy, we will just decode it immediately. */
-    
     {
         /* Allocate space for the decoded message. */
         simple_message_t message = SIMPLE_MESSAGE_INIT_ZERO;
-        
+
         /* Create a stream that reads from the buffer. */
         pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
-        
+
         /* Now we are ready to decode the message. */
         status = pb_decode(&stream, SIMPLE_MESSAGE_FIELDS, &message);
-        
+
         /* Check for errors... */
         if (!status)
         {
             printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
             return 1;
         }
-        
+
         /* Print the data contained in the message. */
         printf("Your lucky number was %d!\n", (int)message.lucky_number);
     }
-    
+
     return 0;
 }
 
