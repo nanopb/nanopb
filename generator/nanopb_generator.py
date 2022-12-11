d7b99225ecf8a830712787ae5141242e4e465dd0
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2022-12-11 12:54:08 +0200

    Setting version to 0.4.8-dev

--- generator/nanopb_generator.py
+++ generator/nanopb_generator.py
@@ -4,7 +4,7 @@
 from __future__ import unicode_literals
 
 '''Generate header file for nanopb from a ProtoBuf FileDescriptorSet.'''
-nanopb_version = "nanopb-0.4.7"
+nanopb_version = "nanopb-0.4.8-dev"
 
 import sys
 import re
