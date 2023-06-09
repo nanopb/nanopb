bf706840b4fc91121210eff8195f8d2ea6ee99b0
jheaff1 <jheaff1@outlook.com>
2023-06-09 14:15:45 +0100

    Add unit test for int_size option for enums

---
+++ tests/enum_sizes/enumsizes_intsize_unittests.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "enumsizes.pb.h"
+#include "unittests.h"
+
+extern "C" int TestIntSize() {
+  int status = 0;
+
+  TEST(sizeof(IntSizeInt8) == sizeof(uint8_t));
+  TEST(sizeof(IntSizeInt16) == sizeof(uint16_t));
+  TEST(sizeof(IntSizeInt32) == sizeof(uint32_t));
+  TEST(sizeof(IntSizeInt64) == sizeof(uint64_t));
+
+  if (status != 0) fprintf(stdout, "\n\nSome tests FAILED!\n");
+
+  return status;
+}
