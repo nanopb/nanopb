f2489d516f5426b9d048883ae8005e5df8ea88ee
Anthony Pesch <inolen@gmail.com>
2024-11-15 03:34:21 -0500

    Refactored pb_decode to be a wrapper around pb_decode_ex

--- pb_decode.c
+++ pb_decode.c
@@ -1192,16 +1192,7 @@ bool checkreturn pb_decode_ex(pb_istream_t *stream, const pb_msgdesc_t *fields, 
 
 bool checkreturn pb_decode(pb_istream_t *stream, const pb_msgdesc_t *fields, void *dest_struct)
 {
-    bool status;
-
-    status = pb_decode_inner(stream, fields, dest_struct, 0);
-
-#ifdef PB_ENABLE_MALLOC
-    if (!status)
-        pb_release(fields, dest_struct);
-#endif
-
-    return status;
+    return pb_decode_ex(stream, fields, dest_struct, 0);
 }
 
 #ifdef PB_ENABLE_MALLOC
