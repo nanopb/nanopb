c71df2e2989967ebfb70fec4980d69da12b91d09
weichslgartner <weichslgartner@gmail.com>
2025-01-10 15:42:43 +0100

    add version generation for CPE and PURL in zephyr/module.yml
    Signed-off-by: weichslgartner <weichslgartner@gmail.com>

--- tools/set_version.sh
+++ tools/set_version.sh
@@ -8,6 +8,8 @@ VERSION_NUMBER_ONLY=$(echo $1 | cut -d '-' -f 2)
 sed -i -e 's/nanopb_version\s*=\s*"[^"]*"/nanopb_version = "'$1'"/' generator/nanopb_generator.py
 sed -i -e 's/#define\s*NANOPB_VERSION\s*.*/#define NANOPB_VERSION "'$1'"/' pb.h
 sed -i -e 's/project(\s*nanopb\s*VERSION\s*[^)]*\s*LANGUAGES\s*C\s*)/project(nanopb VERSION '$VERSION_NUMBER_ONLY' LANGUAGES C)/' CMakeLists.txt
+sed -i -e 's/nanopb\@[^\s]*/nanopb\@'$VERSION_NUMBER_ONLY'/' zephyr/module.yml 
+sed -i -e 's/cpe:2.3:a:nanopb_project:nanopb:[^:]*/cpe:2.3:a:nanopb_project:nanopb:'$VERSION_NUMBER_ONLY'/' zephyr/module.yml 
 # Update the first occurrence of "version" in MODULE.bazel, which is the nanopb
 # version. Use awk instead of sed because there is no sed approach that works on
 # both Linux and MacOS (https://stackoverflow.com/q/148451/24291280).
