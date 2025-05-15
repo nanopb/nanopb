761b42a8cff839e3483579b8a64855fd6c8d06f7
Mark Schulte <mark@gochromatic.com>
2025-05-15 17:29:05 -0400

    fixup: build error

--- tests/message_sizes/dummy.c
+++ tests/message_sizes/dummy.c
@@ -5,13 +5,7 @@
 int main()
 {
     PB_STATIC_ASSERT(MESSAGES2_PB_H_MAX_SIZE == xmit_size, INCORRECT_MAX_SIZE);
-    // Computed 13 bytes by waiting for compilation error.
-    // Purpose is to check that if a message has a submessage callback,
-    // the size of the message can still be computed.
     PB_STATIC_ASSERT(13 == WithCallbackSubmsg_size, INCORRECT_MAX_SIZE);
-    // Computed 13 bytes by waiting for compilation error.
-    // Purpose is to check that if a message has a type FT_POINTER,
-    // the size of the message can still be computed.
     PB_STATIC_ASSERT(13 == WithPointerSubmsg_size, INCORRECT_MAX_SIZE);
     return xmit_size;
 }
