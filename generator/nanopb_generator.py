038e24252e86825c787fe4bd66f1f46c8c9fe9e1
Michael Hordijk <hordijk@aurora.tech>
2025-06-19 12:39:20 -0600

    C++ descriptor size needs to be guarded
    The define that is being used to define the C++ descriptor size may itself
    be undefined due to various guards.  Make sure it is available before
    defining the C++ size.

--- generator/nanopb_generator.py
+++ generator/nanopb_generator.py
@@ -1566,7 +1566,10 @@ class Message(ProtoElement):
 
         size_define = "%s_size" % (self.name)
         if size_define in local_defines:
+            result += '/* The size define we are using may be defined conditionally guarded. */\n'
+            result += '#if defined %s\n' % size_define
             result += '    static PB_INLINE_CONSTEXPR const pb_size_t size = %s;\n' % (size_define)
+            result += '#endif\n'
 
         result += '    static PB_INLINE_CONSTEXPR const pb_msgdesc_t* fields() {\n'
         result += '        return &%s_msg;\n' % (self.name)
