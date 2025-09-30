fee3caf3e30c0ee3ba8c2addc531df0e5d662539
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2025-09-30 20:40:13 +0300

    Add testcase for cxx descriptors with naming style (#1120)

---
+++ tests/namingstyle/test_cxx_descriptor.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "naming_style.pb.h"
+#include "unittests.h"
+
+extern "C" int main() {
+  using namespace nanopb;
+
+  int status = 0;
+
+  TEST(MessageDescriptor<main_message_t>::fields_array_length ==
+       main_message_t_msg.field_count);
+
+  if (status != 0) fprintf(stdout, "\n\nSome tests FAILED!\n");
+
+  return status;
+}
