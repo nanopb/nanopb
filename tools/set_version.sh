66ac2beac565bdb4a7e15294712ce89420552aff
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2026-01-28 14:36:01 +0200

    Use semantic versions for version numbers (#1012)

--- tools/set_version.sh
+++ tools/set_version.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
 # Run this from the top directory of nanopb tree.
-# e.g. user@localhost:~/nanopb$ tools/set_version.sh nanopb-0.1.9-dev
+# e.g. user@localhost:~/nanopb$ tools/set_version.sh 1.0.0-dev
 # It sets the version number in pb.h and generator/nanopb_generator.py.
+#
+# VERSION_NUMBER is e.g. "1.0.0-dev" (valid semver)
+# WITHOUT_SUFFIX is just "1.0.0" (only numbers)
+# MAJOR_VERSION is "1"
 
-VERSION_NUMBER_ONLY=$(echo $1 | cut -d '-' -f 2)
-sed -i -e 's/nanopb_version\s*=\s*"[^"]*"/nanopb_version = "'$1'"/' generator/nanopb_generator.py
-sed -i -e 's/#define\s*NANOPB_VERSION\s*.*/#define NANOPB_VERSION "'$1'"/' pb.h
-sed -i -e 's/project(\s*nanopb\s*VERSION\s*[^)]*\s*LANGUAGES\s*C\s*)/project(nanopb VERSION '$VERSION_NUMBER_ONLY' LANGUAGES C)/' CMakeLists.txt
-sed -i -e "0,/version: '[^']*'/s/version: '[^']*'/version: '$VERSION_NUMBER_ONLY'/" meson.build
-sed -i -e 's/nanopb\@[^\s]*/nanopb\@'$VERSION_NUMBER_ONLY'/' zephyr/module.yml 
-sed -i -e 's/cpe:2.3:a:nanopb_project:nanopb:[^:]*/cpe:2.3:a:nanopb_project:nanopb:'$VERSION_NUMBER_ONLY'/' zephyr/module.yml 
+VERSION_NUMBER=$1
+WITHOUT_SUFFIX=$(echo $1 | cut -d '-' -f 1)
+MAJOR_VERSION=$(echo $1 | cut -d '.' -f 1)
+SEMVER_VALID=$(echo $1 | sed -nE '/^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?(\+([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?$/p')
+
+if [[ "x$1" != "x$SEMVER_VALID" ]] || [[ "x$1" == "x" ]]
+then echo "Error: version number must follow semantic version format"
+exit 1
+fi
+
+# Use semantic version where supported
+sed -i -e 's/nanopb_version\s*=\s*"[^"]*"/nanopb_version = "nanopb-'$VERSION_NUMBER'"/' generator/nanopb_generator.py
+sed -i -e 's/#define\s*NANOPB_VERSION\s*.*/#define NANOPB_VERSION "nanopb-'$VERSION_NUMBER'"/' pb.h
+sed -i -e "0,/version: '[^']*'/s/version: '[^']*'/version: '$VERSION_NUMBER'/" meson.build
+sed -i -e 's/nanopb\@[^\s]*/nanopb\@'$VERSION_NUMBER'/' zephyr/module.yml
+sed -i -e 's/cpe:2.3:a:nanopb_project:nanopb:[^:]*/cpe:2.3:a:nanopb_project:nanopb:'$VERSION_NUMBER'/' zephyr/module.yml
+sed -i -e 's/"version":\s*"[^"]*"/"version": "'$VERSION_NUMBER'"/' library.json
+sed -i -e 's/version =.*/version = "'$VERSION_NUMBER'"/' conanfile.py
+
+# CMake doesn't support semver suffixes, but there is separate VERSION_STRING where we can have them.
+sed -i -e 's/project(\s*nanopb \s*VERSION \s*[^)]*\s*LANGUAGES\s*C\s*)/project(nanopb VERSION '$WITHOUT_SUFFIX' LANGUAGES C)/' CMakeLists.txt
+sed -i -e 's/set(\s*nanopb_VERSION_STRING \s*[^)]*)/set(nanopb_VERSION_STRING "nanopb-'$VERSION_NUMBER'")/' CMakeLists.txt
+sed -i -e 's/set(\s*nanopb_VERSION \s*[^)]*)/set(nanopb_VERSION "'$VERSION_NUMBER'")/' CMakeLists.txt
+sed -i -e 's/set(\s*nanopb_SOVERSION \s*[^)]*)/set(nanopb_SOVERSION '$MAJOR_VERSION')/' CMakeLists.txt
+
+# Python versioning does not support semantic version suffixes,
+# but supports .devN suffix.
+if [[ $VERSION_NUMBER != $WITHOUT_SUFFIX ]]
+then sed -i -e 's/^version =.*/version = "'$WITHOUT_SUFFIX.dev0'"/' extra/poetry/pyproject.toml
+else sed -i -e 's/^version =.*/version = "'$WITHOUT_SUFFIX'"/' extra/poetry/pyproject.toml
+fi
+
 # Update the first occurrence of "version" in MODULE.bazel, which is the nanopb
 # version. Use awk instead of sed because there is no sed approach that works on
 # both Linux and MacOS (https://stackoverflow.com/q/148451/24291280).
-awk '/version/ && !done { gsub(/version = ".*"/, "version = \"'$VERSION_NUMBER_ONLY'\""); done=1}; 1' MODULE.bazel > temp.MODULE.bazel && mv temp.MODULE.bazel MODULE.bazel
+awk '/version/ && !done { gsub(/version = ".*"/, "version = \"'$VERSION_NUMBER'\""); done=1}; 1' MODULE.bazel > temp.MODULE.bazel && mv temp.MODULE.bazel MODULE.bazel
 
-VERSION_ONLY=$(echo $1 | sed 's/nanopb-//')
-if [[ $1 != *dev ]]
-then sed -i -e 's/"version":\s*"[^"]*"/"version": "'$VERSION_ONLY'"/' library.json
-fi
-
-sed -i -e 's/version =.*/version = "'$VERSION_ONLY'"/' conanfile.py
-sed -i -e 's/^version =.*/version = "'$VERSION_ONLY'"/' extra/poetry/pyproject.toml
