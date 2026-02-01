5b4b0d1bea4b51ba4fabdf9c9533e5dac8b24393
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2026-02-01 12:18:13 +0200

    Reorganize test cases for new feature enable macros
    The testing of different feature combinations will be moved
    to workflow scripts that run scons with different arguments.
    
    For example the previously separated test cases for malloc / no malloc
    will be replaced with the default build having malloc, and separate
    testing with: scons PBFLAGS="PB_NO_MALLOC".
    
    Tests that require malloc would then be automatically skipped.
    This commit reorganizes the SConscript files but does not yet
    skip tests.

--- tests/common/malloc_wrappers.h
+++ tests/common/malloc_wrappers.h
@@ -1,4 +1,11 @@
+#ifndef MALLOC_WRAPPERS_H
+#define MALLOC_WRAPPERS_H
+
 #include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void* malloc_with_check(size_t size);
 void free_with_check(void *mem);
@@ -8,3 +15,12 @@ size_t get_allocation_size(const void *mem);
 size_t get_alloc_bytes();
 void set_max_alloc_bytes(size_t max_bytes);
 size_t get_max_alloc_bytes();
+
+#define pb_realloc(ptr,size) realloc_with_check(ptr,size)
+#define pb_free(ptr) free_with_check(ptr)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
